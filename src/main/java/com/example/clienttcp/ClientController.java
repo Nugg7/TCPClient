0220d9c937bc94161c96a397bc9553723555b755
nuggs <steven.guevarra.1@gmail.com>
2024-04-02 15:30:59 +0200

    added comments to explain various functions of code

--- src/main/java/com/example/clienttcp/ClientController.java
+++ src/main/java/com/example/clienttcp/ClientController.java
@@ -26,27 +26,29 @@ public class ClientController{
     String user;
 
     public void submit(ActionEvent event) throws IOException {
+        //connects to server and sends the profile to be saved in the server
         JSONManager json = new JSONManager();
-        user = UserName.getText();
+        user = UserName.getText(); //gets the username from the text field in the scene
         Client client = null;
         try{
             Socket socket = new Socket("localhost", 1234); //generates socket (ip address, port)
-            client = new Client(socket, user);
-            json.setProfile(user, client.getUuid());
+            client = new Client(socket, user); //conntects to the server through the socket
+            json.setProfile(user, client.getUuid()); //sets the user and the UUID in the json file
             client.sendMessage(json.getProfile().toString()); //sends profile to server (username and UUID)
             System.out.println("connected to server");
         }catch (IOException e){
             e.printStackTrace();
         }
-        System.out.println(json.getProfile());
+        System.out.println(json.getProfile()); //debug to see the username and UUID in console
 
+        //after sign in changes to auction scene
         Parent root = FXMLLoader.load(getClass().getResource("Auction-view.fxml"));
         Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-
+    //onclick event to close the program with quit button in auction scene
     public void quit(ActionEvent event){
         Platform.exit();
     }
